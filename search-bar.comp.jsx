17977df5a6788de05a7c82b18d7e93810188fdee
hoowercoloradomejia <62470818+hoowercoloradomejia@users.noreply.github.com>
2021-04-18 21:05:46 -0500

    Add files via upload

---
+++ search-bar.comp.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './search-bar.style.css';
+
+const SearchBar = () => {
+    return (
+        <div className="search-bar-container">
+            <div>
+            <input type="text" placeholder="Search..." className="search-input"/>
+            </div>
+            <div>
+            <input type="checkbox"/> Only show products in stock
+            </div>
+            
+            
+        </div>
+    );
+    
+}
+
+export default SearchBar;
